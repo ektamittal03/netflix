fb262baeae03da38759dd134ae32c73a95141924
Ekta Dingliwala <ektadingliwala@Ektas-MacBook-Pro.local>
2022-08-08 15:00:10 +0530

    msg

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,41 +1,50 @@
-import "./navbar.css" ;
+import "./navbar.scss" ;
 import SearchIcon from '@mui/icons-material/Search';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import {useState} from "react";
 
 
 const Navbar = () => {
-  return (
-    <div className="container">
-        <div className="left">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
-            alt=""
-          />
-          <span>Homepage</span>
-          <span>Series</span>
-          <span>Movies</span>
-          <span>New and Popular</span>
-          <span>My List</span>
+    const [isScrolled, setIsScrolled] = useState(false);
+  
+    window.onscroll = () => {
+      setIsScrolled(window.pageYOffset === 0 ? false : true);
+      return () => (window.onscroll = null);
+    };
+    return (
+      <div className={isScrolled ? "navbar scrolled" : "navbar"}>
+        <div className="container">
+          <div className="left">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
+              alt=""
+            />
+            <span>Homepage</span>
+            <span>Series</span>
+            <span>Movies</span>
+            <span>New and Popular</span>
+            <span>My List</span>
+          </div>
+          <div className="right">
+            <SearchIcon className="icon" />
+            <span>KID</span>
+            <NotificationsIcon className="icon" />
+            <img
+              src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+              alt=""
+            />
+            <div className="profile">
+              <ArrowDropDownIcon className="icon" />
+              <div className="options">
+                <span>Settings</span>
+                <span>Logout</span>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="right">
-      
-          <SearchIcon classname="icons"/>
-          <span>KIDS</span>
-          <NotificationsIcon classname="icons"/>
-          <img src="https://thumbs.dreamstime.com/b/flowers-4999705.jpg" alt="" />
-          <div classname="profile" >
-          <ArrowDropDownIcon classname="icons"/>
-          <div classname="options">
-            <span>Settings</span>
-            <span>Logout</span>
-
-          </div>
-          </div>
-
-          </div>
-          </div>
-  );
-}
-
-export default Navbar
+      </div>
+    );
+  };
+  
+  export default Navbar;
--- src/home/home.js
+++ src/home/home.js
@@ -1,10 +1,12 @@
 import Navbar from "../components/navbar/navbar"
+import Features from "../components/features/features"
 import "./home.css"
 
 const Home = () => {
   return (
     <div className="home">
       <Navbar />
+      <Features type="movies"/>
     </div>
   )
 }
